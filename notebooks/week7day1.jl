412e6810f5a856768dc52b74c0c6f2eea35b4a7b
Eric Ford <ebf11@psu.edu>
2022-10-03 11:10:59 -0400

    wk7 day 1 finalized

--- notebooks/week7day1.jl
+++ notebooks/week7day1.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.12
+# v0.19.11
 
 using Markdown
 using InteractiveUtils
@@ -15,7 +15,7 @@ end
 
 # ╔═╡ 4dade109-dd8b-450c-8a79-b0e5f8b9304d
 md"""
-# Exoplanet Masses & Orbits
+# Your Questions and Exoplanet Masses & Orbits
 **Astro 497, Week 7, Day 1**
 """
 
@@ -27,12 +27,83 @@ md"""
 # Questions (general)
 """
 
-# ╔═╡ ec125edc-5716-423d-8b15-42df6fa0921c
+# ╔═╡ 4606cfbb-de3b-4ff7-aa10-59fed7ebdcf8
+LocalResource("../_assets/week2/circular_diagram.png")
 
+# ╔═╡ 0a43f2f3-059b-4812-8dd1-1cc872255b76
+md"""
+Transit duration ($e=0$):  
+```math
+\begin{eqnarray}
+T & \simeq & 13\, \mathrm{hr} \left(\frac{P}{365\,\mathrm{d}}\right)^{1/3} \left(\frac{\rho_\odot}{\rho_\star}\right)^{1/3} \sqrt{1-b^2} \\
+& \simeq & 4.5\, \mathrm{hr} \left(\frac{P}{4\,\mathrm{d}}\right)^{1/3} \left(\frac{\rho_\odot}{\rho_\star}\right)^{1/3} \sqrt{1-b^2}$
+\end{eqnarray}
+```
+
+Fraction of time in transit: 
+```math
+\begin{eqnarray}
+\frac{T}{P} & \simeq{} & \simeq 0.0015 \left(\frac{P}{365\,\mathrm{d}}\right)^{-2/3} \left(\frac{\rho_\odot}{\rho_\star}\right)^{1/3} \sqrt{1-b^2} \\
+& \simeq{} &  0.045  \left(\frac{P}{4\,\mathrm{d}}\right)^{-2/3} \left(\frac{\rho_\odot}{\rho_\star}\right)^{1/3} \sqrt{1-b^2} 
+\end{eqnarray}
+```
+"""
+
+# ╔═╡ eef26397-4188-4087-8a80-d3842c0a4a9b
+#= md"""
+Duration of transiting another plannet relative to duration of transit of host star
+$T_{pl} / T_{\star} \simeq \left(\frac{R_{p}}{R_\star}\right) \left(\frac{\sqrt{1-b_{pl}^2}}{\sqrt{1-b_\star^2}}\right)$
+"""
+=#
+
+# ╔═╡ b0660d1f-01bb-4084-ac2d-783151d86127
+hint(md"""
+Other factors that affect probability of mutual transits:
+- Size of planets
+- Mutual orbital inclination
+- Relative orbital motion of the two planets 
+- Ratio of orbital periods
+""")
+
+# ╔═╡ 767895fd-d98c-47ed-b141-d2959b274788
+md"""
+$\mathrm{MLE} = \mathrm{argmax}_\theta  \; p(\mathrm{data} | \theta)$
+
+$\mathrm{MAP} = \mathrm{argmax}_\theta  \; p(\theta|\mathrm{data})$
+
+$\mathrm{posterior} = \frac{(\mathrm{prior}) (\mathrm{likelihood)}}{(\mathrm{normalization\,constant})}$
+
+$p(\theta | \mathrm{data}) = \frac{p(\theta) p(\mathrm{data} | \theta) }{\int \, p(\theta) p(\mathrm{data} | \theta) \, d\theta}$
+
+
+"""
 
 # ╔═╡ 158e3b44-1c58-459e-ab48-d35c3ef90f05
 md"""
 # Questions (exam specific)
+"""
+
+# ╔═╡ 9696bff1-5eb2-4adc-b5d2-3637e3a819e6
+md"""
+- RV(t)
+- Depth, duration, and probability of transits
+- Magnitude of RM effect
+- TTVs: qualitative behavior and what variables the amplitude and timescale are most sensitive to 
+"""
+
+# ╔═╡ e520813f-7125-4acc-b75b-9a8d98d004bd
+md"""
+- Mostly qualitatively
+- Could use [TTVFaster calculator](https://psuastro497.github.io/Fall2022/notebooks/week3day3/) if need to.
+- Could compare precession rate due to GR (from reading) to another timescale (e.g., orbital period).
+
+"""
+
+# ╔═╡ 15d6dbb6-2996-4357-9829-4e2f0d3a69e4
+md"""
+- In most cases, there is a way to do the problem using arithmetic you could do in your head (e.g., if you remember the values for the Earth-Sun or a Hot-Jupiter and use scaling relations), perhaps using  one simple calculation ($\sqrt{x}$ or $x^{1/3}$).  
+- You're certainliy welcome to use scratch paper, a calculator, or Pluto notebook
+- Can also use interactive widges from labs or class (especially if you use expressions with mks/cgs units).
 """
 
 # ╔═╡ 1ea3b5f3-5eed-4452-ae90-df5cdb4b3b99
@@ -103,6 +174,11 @@ $(ThreeColumn(
 # ╔═╡ 745e7b26-8db6-48b1-b20d-9d1093981fa1
 LocalResource("../_assets/week5/Compare_models/Models_Compare_underlying_multiplicities.png", :width=>"75%")
 
+# ╔═╡ e5f85e8e-58aa-4480-8780-ed571a3e45a7
+md"""
+## Eccentricities & Inclinations of Multiple Planet Systems
+"""
+
 # ╔═╡ 6bbd37e2-98dc-4c44-95ef-e236f143b942
 LocalResource("../_assets/week5/Best_model/Clustered_P_R_Model_underlying_pratio_min_vs_amd_ecc_incl.png")
 
@@ -144,14 +220,14 @@ What are the main differences between the maximum a posteriori and the maximum l
 # ╔═╡ 5e41242d-cf26-4573-b991-3ccb0f8de985
 question(md"Are there external factors, such as dust clouds, gravitational lensing, or overlapping solar systems, that can significantly affect collected data?")
 
-# ╔═╡ 2489ac66-fad3-4928-8f30-984ed984dfc3
-question(md"""How extensive are the numerical calculations going to be?""")
-
 # ╔═╡ 6213c69e-d73d-43aa-bf27-3b89969b915c
 question(md"""Are there certain equations we should pay more attention to when studying?""")
 
 # ╔═╡ 2f1b6b56-a016-44ae-aadb-2de0eee551bb
 question(md"To what level of detail do we need to understand non-Keplerian motion? Qualitatively or Quantitatively?")
+
+# ╔═╡ 2489ac66-fad3-4928-8f30-984ed984dfc3
+question(md"""How extensive are the numerical calculations going to be?""")
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -169,9 +245,8 @@ PlutoUI = "~0.7.40"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.8.2"
+julia_version = "1.7.1"
 manifest_format = "2.0"
-project_hash = "06d50ac5a623a4175d1f10ff59cc0cdec47248c1"
 
 [[deps.AbstractPlutoDingetjes]]
 deps = ["Pkg"]
@@ -181,7 +256,6 @@ version = "1.1.4"
 
 [[deps.ArgTools]]
 uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
-version = "1.1.1"
 
 [[deps.Artifacts]]
 uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
@@ -204,7 +278,6 @@ version = "0.11.4"
 [[deps.CompilerSupportLibraries_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
-version = "0.5.2+0"
 
 [[deps.Dates]]
 deps = ["Printf"]
@@ -215,9 +288,8 @@ deps = ["Random", "Serialization", "Sockets"]
 uuid = "8ba89e20-285c-5b6f-9357-94700520ee1b"
 
 [[deps.Downloads]]
-deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
+deps = ["ArgTools", "LibCURL", "NetworkOptions"]
 uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
-version = "1.6.0"
 
 [[deps.FileWatching]]
 uuid = "7b1f6079-737a-58dc-b8bc-7a2ca5c1b5ee"
@@ -282,12 +354,10 @@ version = "0.15.17"
 [[deps.LibCURL]]
 deps = ["LibCURL_jll", "MozillaCACerts_jll"]
 uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
-version = "0.6.3"
 
 [[deps.LibCURL_jll]]
 deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
 uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
-version = "7.84.0+0"
 
 [[deps.LibGit2]]
 deps = ["Base64", "NetworkOptions", "Printf", "SHA"]
@@ -296,7 +366,6 @@ uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
 [[deps.LibSSH2_jll]]
 deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
 uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
-version = "1.10.2+0"
 
 [[deps.Libdl]]
 uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
@@ -327,23 +396,19 @@ uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
 [[deps.MbedTLS_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
-version = "2.28.0+0"
 
 [[deps.Mmap]]
 uuid = "a63ad114-7e13-5084-954f-fe012c677804"
 
 [[deps.MozillaCACerts_jll]]
 uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
-version = "2022.2.1"
 
 [[deps.NetworkOptions]]
 uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
-version = "1.2.0"
 
 [[deps.OpenBLAS_jll]]
 deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
 uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
-version = "0.3.20+0"
 
 [[deps.OrderedCollections]]
 git-tree-sha1 = "85f8e6578bf1f9ee0d11e7bb1b1456435479d47c"
@@ -359,7 +424,6 @@ version = "2.4.0"
 [[deps.Pkg]]
 deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
 uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
-version = "1.8.0"
 
 [[deps.PlutoHooks]]
 deps = ["InteractiveUtils", "Markdown", "UUIDs"]
@@ -381,9 +445,9 @@ version = "0.2.3"
 
 [[deps.PlutoUI]]
 deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "Markdown", "Random", "Reexport", "UUIDs"]
-git-tree-sha1 = "a602d7b0babfca89005da04d89223b867b55319f"
+git-tree-sha1 = "2777a5c2c91b3145f5aa75b61bb4c2eb38797136"
 uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
-version = "0.7.40"
+version = "0.7.43"
 
 [[deps.Printf]]
 deps = ["Unicode"]
@@ -416,7 +480,6 @@ version = "3.4.0"
 
 [[deps.SHA]]
 uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
-version = "0.7.0"
 
 [[deps.Serialization]]
 uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
@@ -435,12 +498,10 @@ uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
 [[deps.TOML]]
 deps = ["Dates"]
 uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
-version = "1.0.0"
 
 [[deps.Tar]]
 deps = ["ArgTools", "SHA"]
 uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
-version = "1.10.1"
 
 [[deps.Test]]
 deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
@@ -461,22 +522,18 @@ uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
 [[deps.Zlib_jll]]
 deps = ["Libdl"]
 uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
-version = "1.2.12+3"
 
 [[deps.libblastrampoline_jll]]
 deps = ["Artifacts", "Libdl", "OpenBLAS_jll"]
 uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
-version = "5.1.1+0"
 
 [[deps.nghttp2_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
-version = "1.48.0+0"
 
 [[deps.p7zip_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
-version = "17.4.0+0"
 """
 
 # ╔═╡ Cell order:
@@ -484,14 +541,21 @@ version = "17.4.0+0"
 # ╟─c8f40951-6a14-4b0a-90f5-48206b77f123
 # ╟─b264c46c-43de-49c2-8788-08e192f25ff3
 # ╟─30bb5f69-d7ca-472e-9868-2ef5f8325c22
+# ╟─4606cfbb-de3b-4ff7-aa10-59fed7ebdcf8
 # ╟─976efdd7-992c-4559-90a3-82a3b4f7ef3e
+# ╟─0a43f2f3-059b-4812-8dd1-1cc872255b76
+# ╟─eef26397-4188-4087-8a80-d3842c0a4a9b
+# ╟─b0660d1f-01bb-4084-ac2d-783151d86127
 # ╟─1888dcd2-62e6-4556-a2ae-654e7000ad46
+# ╟─767895fd-d98c-47ed-b141-d2959b274788
 # ╟─5e41242d-cf26-4573-b991-3ccb0f8de985
-# ╠═ec125edc-5716-423d-8b15-42df6fa0921c
 # ╟─158e3b44-1c58-459e-ab48-d35c3ef90f05
-# ╟─2489ac66-fad3-4928-8f30-984ed984dfc3
 # ╟─6213c69e-d73d-43aa-bf27-3b89969b915c
+# ╟─9696bff1-5eb2-4adc-b5d2-3637e3a819e6
 # ╟─2f1b6b56-a016-44ae-aadb-2de0eee551bb
+# ╟─e520813f-7125-4acc-b75b-9a8d98d004bd
+# ╟─2489ac66-fad3-4928-8f30-984ed984dfc3
+# ╟─15d6dbb6-2996-4357-9829-4e2f0d3a69e4
 # ╟─1ea3b5f3-5eed-4452-ae90-df5cdb4b3b99
 # ╟─32e6170f-0ab3-4681-9517-f9ef940e2b30
 # ╟─2849d543-08df-4abd-9da6-a5fe02bc9228
@@ -501,13 +565,14 @@ version = "17.4.0+0"
 # ╟─4480d4e1-324c-4414-b278-6a798bab77f0
 # ╟─cf7ef986-66a6-4ba9-b1bd-2291862fb3f0
 # ╟─745e7b26-8db6-48b1-b20d-9d1093981fa1
+# ╟─e5f85e8e-58aa-4480-8780-ed571a3e45a7
 # ╟─6bbd37e2-98dc-4c44-95ef-e236f143b942
 # ╟─9566be82-268b-44e6-b5c7-839d0ff49cbf
 # ╟─44db6c55-4235-4ff5-b391-847ac9d369e5
 # ╟─51f670ae-da32-475d-b59e-272edf5c8596
 # ╟─c879333a-5aa4-4528-9110-00f491a9ed4a
-# ╠═fbf647a7-b2ba-445d-8622-35c6cb2e27d0
-# ╠═cda49574-3843-11ed-255d-f301e028a76e
+# ╟─fbf647a7-b2ba-445d-8622-35c6cb2e27d0
+# ╟─cda49574-3843-11ed-255d-f301e028a76e
 # ╟─1d254576-2985-4f2a-8ca3-3c9c0e3960db
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
