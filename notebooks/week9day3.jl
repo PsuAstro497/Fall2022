d29d1da7799c6e5544ef913aeec9d4fec3dbeec0
Eric Ford <ebf11@psu.edu>
2022-10-19 10:01:39 -0400

    add transmission spectra gallery

--- notebooks/week9day3.jl
+++ notebooks/week9day3.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.12
+# v0.19.11
 
 using Markdown
 using InteractiveUtils
@@ -13,10 +13,28 @@ md"""
 **Astro 497, Week 9, Friday**
 """
 
+# ╔═╡ da5a69e7-235a-4f4e-b404-c8934f1a3743
+TableOfContents()
+
 # ╔═╡ adb15016-e599-40eb-8a57-ebe7854ebe97
 md"""
 $(LocalResource("../_assets/week9/fig1_transit_geom.png"))
 --- [Kreidberg, chapter from Handbook of Exoplanets](https://arxiv.org/abs/1709.05941)
+"""
+
+# ╔═╡ 92b3b41d-01fc-4fb5-909f-9a933ca211e6
+md"""
+
+## Example Transmission Spectrum of WASP-43b 
+$(LocalResource("../_assets/week9/fig3_w43.png",:width=>"80%"))
+- Main figure is transmission spectrum from Hubble WFC3
+- Inset figure is emission spectrum is Spitzer/IRAC 3.6 & 4.5μm
+--- [Kreidberg et al. 2015 & chapter from Handbook of Exoplanets](https://arxiv.org/abs/1709.05941)
+"""
+
+# ╔═╡ 2c723044-9c7c-45f3-9600-29275938f9af
+md"""
+# What sets the scale for transmission spectroscopy signal?
 """
 
 # ╔═╡ 1c92299c-fd1d-421b-a0c2-c840e8e06062
@@ -28,6 +46,10 @@ $$H = \frac{k_B T}{\mu_m g}$$
 - Mean molecular mass: $\mu_m$ 
 - Gravitational acceleration: $g$ 
 - Boltzmann constant: $k_B$ 
+
+Assumes an **isothermal** atmosphere in hydrostatic equilbirum.
+
+$$\rho(z) = \rho_0 \exp(-z/H)$$
 """
 
 # ╔═╡ fb614745-bf3d-45f7-b6a3-9d81dfdb5045
@@ -57,21 +79,29 @@ md"""
 | Earth | $\simeq 10^{-4}$ | 273 | 10 | 28 | $\simeq 10^{-6}$ |
 """
 
-# ╔═╡ 92b3b41d-01fc-4fb5-909f-9a933ca211e6
+# ╔═╡ 4f07a97e-b844-4f5c-b15f-1432ded4735a
 md"""
+# Gallery of Transmission Spectroscopy measurements
 
-### Spectra of WASP-43b (hot-Jupiter)
-$(LocalResource("../_assets/week9/fig3_w43.png",:width=>"80%"))
-- Main figure is transmission spectrum from Hubble WFC3
-- Inset figure is emission spectrum is Spitzer/IRAC 3.6 & 4.5μm
---- [Kreidberg et al. 2015 & chapter from Handbook of Exoplanets](https://arxiv.org/abs/1709.05941)
+$(LocalResource("../_assets/week9/atmospheric_scaleheight.jpeg", :width=>"100%"))
+
+--- [Archive fo Exoplanet Transmission Spectra](https://stellarplanet.org/science/exoplanet-transmission-spectra/) & Wakeford (2020)
+"""
+
+# ╔═╡ 0d3a2df4-946a-4c31-b7d4-53b94a980157
+md"""
+#### Things to look for
+- Sodium absorption ~0.6μm (HAT-P-1)
+- Rayleigh scattering (HD 189733b)
+- Lack of rayleigh scattering → clouds (GJ 436b, GJ 1214b)
+- Water Absorption ~1.4μm (WASP-127b, WASP-39b, WASP-107b, WASP-52b, HAT-P26b)
+- Lack of features (TRAPPIST)
 """
 
 # ╔═╡ ae0e2eb9-434c-4e9d-a5a2-83d2c867c5fd
 md"""
+#  How to prioritize planets for detailed atmospheric characterization?
 ## Transmission spectroscopy metric
-- Proportional to SNR for transmission spectroscopy measurement roughly corresponding to JWST's Near IR Imager and Slitless Spectrograph (NIRISS)
-- 
 ```math
 \begin{eqnarray}
 \mathrm{TSM} & = & (\mathrm{Scale\; factor}) \times  \left(\frac{R_p}{R_\oplus}\right)^3 \left(\frac{M_\oplus}{M_p}\right) \left(\frac{R_\odot}{R_\star}\right)^2 \left(\frac{T_{eq}}{K}\right) \times 10^{-m_J/5} \\
@@ -79,10 +109,12 @@ md"""
 \end{eqnarray}
 ```
 
-- apparent magnitude in J band: $m_J$
-- Equilibrium temperature: $T_{eq} \equiv T_{\star,eff} \sqrt{\frac{R_\star}{a}} \left(2^{-1/4}\right)$
+- Proportional to SNR for transmission spectroscopy measurement 
+- Starts with scale height model → assumes isothermal atmosphere with **no clouds or hazes**
+- Adds assumption of equilibrium temperature: $T_{eq} \equiv T_{\star,eff} \sqrt{\frac{R_\star}{a}} \left(2^{-1/4}\right)$
    - assumes zero albedo, full day-night heat redsitribution
-- Assumes no clouds or hazes
+- Original TSM uses Apparent aagnitude in J band: $m_J$ → roughly corresponding to JWST's Near IR Imager and Slitless Spectrograph (NIRISS)
+   - Variations on TSM use apparent magnitude in other bands.
 
 --- [Kempton et al. (2018)](https://ui.adsabs.harvard.edu/abs/2018PASP..130k4401K/abstract)
 
@@ -104,6 +136,9 @@ md"""
 # Setup & Helper Code
 """
 
+# ╔═╡ 5a2ac6e2-6bac-40a7-8d0f-b8300e45f4ae
+ChooseDisplayMode()
+
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
@@ -119,9 +154,8 @@ PlutoUI = "~0.7.43"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.8.2"
+julia_version = "1.7.1"
 manifest_format = "2.0"
-project_hash = "69f995e0dc6a1c35a46792be048dc8abd510ce38"
 
 [[deps.AbstractPlutoDingetjes]]
 deps = ["Pkg"]
@@ -131,7 +165,6 @@ version = "1.1.4"
 
 [[deps.ArgTools]]
 uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
-version = "1.1.1"
 
 [[deps.Artifacts]]
 uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
@@ -154,7 +187,6 @@ version = "0.11.4"
 [[deps.CompilerSupportLibraries_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
-version = "0.5.2+0"
 
 [[deps.Dates]]
 deps = ["Printf"]
@@ -165,9 +197,8 @@ deps = ["Random", "Serialization", "Sockets"]
 uuid = "8ba89e20-285c-5b6f-9357-94700520ee1b"
 
 [[deps.Downloads]]
-deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
+deps = ["ArgTools", "LibCURL", "NetworkOptions"]
 uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
-version = "1.6.0"
 
 [[deps.FileWatching]]
 uuid = "7b1f6079-737a-58dc-b8bc-7a2ca5c1b5ee"
@@ -232,12 +263,10 @@ version = "0.15.17"
 [[deps.LibCURL]]
 deps = ["LibCURL_jll", "MozillaCACerts_jll"]
 uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
-version = "0.6.3"
 
 [[deps.LibCURL_jll]]
 deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
 uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
-version = "7.84.0+0"
 
 [[deps.LibGit2]]
 deps = ["Base64", "NetworkOptions", "Printf", "SHA"]
@@ -246,7 +275,6 @@ uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
 [[deps.LibSSH2_jll]]
 deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
 uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
-version = "1.10.2+0"
 
 [[deps.Libdl]]
 uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
@@ -277,23 +305,19 @@ uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
 [[deps.MbedTLS_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
-version = "2.28.0+0"
 
 [[deps.Mmap]]
 uuid = "a63ad114-7e13-5084-954f-fe012c677804"
 
 [[deps.MozillaCACerts_jll]]
 uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
-version = "2022.2.1"
 
 [[deps.NetworkOptions]]
 uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
-version = "1.2.0"
 
 [[deps.OpenBLAS_jll]]
 deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
 uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
-version = "0.3.20+0"
 
 [[deps.OrderedCollections]]
 git-tree-sha1 = "85f8e6578bf1f9ee0d11e7bb1b1456435479d47c"
@@ -302,14 +326,13 @@ version = "1.4.1"
 
 [[deps.Parsers]]
 deps = ["Dates"]
-git-tree-sha1 = "3d5bf43e3e8b412656404ed9466f1dcbf7c50269"
+git-tree-sha1 = "6c01a9b494f6d2a9fc180a08b182fcb06f0958a0"
 uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
-version = "2.4.0"
+version = "2.4.2"
 
 [[deps.Pkg]]
 deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
 uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
-version = "1.8.0"
 
 [[deps.PlutoHooks]]
 deps = ["InteractiveUtils", "Markdown", "UUIDs"]
@@ -331,9 +354,9 @@ version = "0.2.3"
 
 [[deps.PlutoUI]]
 deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "Markdown", "Random", "Reexport", "UUIDs"]
-git-tree-sha1 = "2777a5c2c91b3145f5aa75b61bb4c2eb38797136"
+git-tree-sha1 = "6e33d318cf8843dade925e35162992145b4eb12f"
 uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
-version = "0.7.43"
+version = "0.7.44"
 
 [[deps.Printf]]
 deps = ["Unicode"]
@@ -366,7 +389,6 @@ version = "3.4.0"
 
 [[deps.SHA]]
 uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
-version = "0.7.0"
 
 [[deps.Serialization]]
 uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
@@ -385,12 +407,10 @@ uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
 [[deps.TOML]]
 deps = ["Dates"]
 uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
-version = "1.0.0"
 
 [[deps.Tar]]
 deps = ["ArgTools", "SHA"]
 uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
-version = "1.10.1"
 
 [[deps.Test]]
 deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
@@ -411,35 +431,36 @@ uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
 [[deps.Zlib_jll]]
 deps = ["Libdl"]
 uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
-version = "1.2.12+3"
 
 [[deps.libblastrampoline_jll]]
 deps = ["Artifacts", "Libdl", "OpenBLAS_jll"]
 uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
-version = "5.1.1+0"
 
 [[deps.nghttp2_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
-version = "1.48.0+0"
 
 [[deps.p7zip_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
-version = "17.4.0+0"
 """
 
 # ╔═╡ Cell order:
 # ╟─e56ca8be-4f08-11ed-0ea5-e554a448992b
+# ╟─da5a69e7-235a-4f4e-b404-c8934f1a3743
 # ╟─adb15016-e599-40eb-8a57-ebe7854ebe97
+# ╟─92b3b41d-01fc-4fb5-909f-9a933ca211e6
+# ╟─2c723044-9c7c-45f3-9600-29275938f9af
 # ╟─1c92299c-fd1d-421b-a0c2-c840e8e06062
 # ╟─fb614745-bf3d-45f7-b6a3-9d81dfdb5045
 # ╟─432c2cc2-d105-4936-a955-15ca4ed1bcc6
-# ╟─92b3b41d-01fc-4fb5-909f-9a933ca211e6
+# ╟─4f07a97e-b844-4f5c-b15f-1432ded4735a
+# ╟─0d3a2df4-946a-4c31-b7d4-53b94a980157
 # ╟─ae0e2eb9-434c-4e9d-a5a2-83d2c867c5fd
 # ╟─80a92749-ef71-4a6e-a1e8-ef14218846d8
 # ╟─390589c9-1769-4a86-95c9-c6ce78438759
 # ╟─e2f9cf86-88cd-4500-a1ea-03015de70296
+# ╟─5a2ac6e2-6bac-40a7-8d0f-b8300e45f4ae
 # ╠═ae9e8a91-f154-4888-b029-b0bdfd4f11de
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
